7681cc89d0ff3443acaccf3ee7d7078856c829da
XxX-KEVIN-XxX <2686191195@qq.com>
2026-07-29 09:33:37 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -136,7 +136,7 @@ async def login_page(request: Request):
 
 @app.get("/index", response_class=HTMLResponse)
 async def map_page(request: Request):
-    return templates.TemplateResponse(request=request, name="index_yuanxian.html")
+    return templates.TemplateResponse(request=request, name="index.html")
 
 
 @app.get("/change-password", response_class=HTMLResponse)
